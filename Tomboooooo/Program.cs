a50af47e76113f6c7197a02fe841866a24097aef
Reka Ehreth <ehreth.reka@gmail.com>
2021-02-08 15:02:54 +0100

    Program reads a line of numbers into an int array (tömb). Then writes the elements of the array.

--- Tomboooooo/Program.cs
+++ Tomboooooo/Program.cs
@@ -6,7 +6,23 @@ namespace Tomboooooo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string alma = "Samsung";
+            //Console.WriteLine(alma[3]);
+            //alma.Split('s');
+            Console.WriteLine("Adj meg 10 pozitív egész számot szóközökkel elválasztva!");
+            string input = Console.ReadLine();
+            string[] szamokStr = input.Split(' ');
+            int[] tombi = new int[10];
+            for (int i = 0; i < szamokStr.Length; i++)
+            {
+                int szam;
+                bool szamE = Int32.TryParse(szamokStr[i], out szam);
+                tombi[i] = szam;
+            }
+            for (int i = 0; i < tombi.Length; i++)
+            {
+                Console.WriteLine("Az " + (i + 1) + ". elem: " + tombi[i]);
+            }
         }
     }
 }
